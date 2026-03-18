0b308c2710c4175e1411300322e94c9c8814a006
Hasko <javics86@gmail.com>
2026-03-18 18:23:30 +0100

    chore(PS-20): :memo: Add TODO of security issue

--- src/contexts/shared/infrastructure/ipfs/IPFS.ts
+++ src/contexts/shared/infrastructure/ipfs/IPFS.ts
@@ -38,8 +38,15 @@ export default class IPFS {
     return network.getJSON<T>(cid);
   }
 
+  // TODO: Fix security issue of this method
   public async getRecord(key: string): Promise<string | undefined> {
     await this.initialize();
+
+    // TODO: This should return multiple records and then validate which one is
+    // correct based on the content. For example, Identities id its its public
+    // key which it's the same as the record and it includes a signatures.
+    // So it's easy to validate if the content has been modified maliciously
+    // For other content, we might need to store a hash of the content
 
     return this.racer.raceGetRecord(this.registry.getAll(), key);
   }
