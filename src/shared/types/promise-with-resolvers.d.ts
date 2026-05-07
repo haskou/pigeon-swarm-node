f00b43a7d2c08c02bca3f595b1c774da4df24506
Hasko <javics86@gmail.com>
2026-05-07 17:52:02 +0200

    refactor: :fire: Remove unused code

--- src/shared/types/promise-with-resolvers.d.ts
+++
@@ -1,9 +0,0 @@
-interface PromiseWithResolvers<T> {
-  promise: Promise<T>;
-  resolve: (value: T | PromiseLike<T>) => void;
-  reject: (reason?: unknown) => void;
-}
-
-interface PromiseConstructor {
-  withResolvers<T>(): PromiseWithResolvers<T>;
-}
